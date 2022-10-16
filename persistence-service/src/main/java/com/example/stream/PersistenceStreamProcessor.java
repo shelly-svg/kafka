353fa77b63ab395af2252a903936628cedaf121b
Denys Tsebro <tsebro.denis@gmail.com>
2022-10-16 12:06:29 +0300

    [persistence-service] implement framework

---
+++ persistence-service/src/main/java/com/example/stream/PersistenceStreamProcessor.java
@@ -0,0 +1,21 @@
+package com.example.stream;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Service
+public class PersistenceStreamProcessor {
+
+    @Bean
+    public Consumer<KStream<String, JsonNode>> processPersistenceStream() {
+        return persistenceStream -> persistenceStream
+            .peek((key, value) -> log.info("Received new message to persist with key: {}, and value: {}", key, value));
+    }
+
+}
