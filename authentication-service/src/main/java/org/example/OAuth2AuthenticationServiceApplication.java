c5949c27c23ccbdf339708b041412559e2d24e19
Denys Tsebro <tsebro.denis@gmail.com>
2023-01-09 22:30:34 +0200

    authentication-service: add framework and implement initial db structure

---
+++ authentication-service/src/main/java/org/example/OAuth2AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2AuthenticationServiceApplication.class, args);
+    }
+
+}
